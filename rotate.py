df29e3ed04cb20f954e4394227f7c436ab113c02
Naimur <naimur58co@gmail.com>
2023-01-25 21:47:38 +0600

    left rotate right shift append

---
+++ rotate.py
@@ -0,0 +1,18 @@
+# left rotate and an elem at idx 3
+b = [11,12,13,14,15,16,17,28]
+n = len(b)
+idx = 3
+for j in range(1):
+    temp = b[j]
+    for i in range(idx):
+        b[i] = b[i+1]
+    b[idx] = temp
+a = []
+for i in b:
+    a += [i]
+# now put a number at index 3
+a += [0]
+for i in range(n + 1 - idx):
+    a[n-i] = a[n-i-1]
+a[idx] = 0
+print(a)
