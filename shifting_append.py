96c866c1f8c0e6715009268e9b82ef3c236cba13
Naimur <naimur58co@gmail.com>
2023-01-25 19:58:14 +0600

    right shifting + append

--- shifting_append.py
+++ shifting_append.py
@@ -1,5 +1,11 @@
-a = [8,9,2,4,5]
-b = []
-for i in a:
-    b+=[i]
-b += [1,2,3]
+# add a an element in index no 4 right shift
+b = [8,9,2,4,5,1,2,3]
+elem = 6
+idx = 4
+i = 1
+n = len(b)
+while i < n - idx:
+    b[n-i] = b[n-i-1]
+    i+=1
+b[idx] = elem
+print(b)
