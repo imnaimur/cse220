cbea5754c04841e2b406a49e8f5112418061a38f
Naimur <naimur58co@gmail.com>
2023-01-27 22:42:27 +0600

    practice

---
+++ oop_practice.py
@@ -0,0 +1,19 @@
+# class Course():
+#     def __init__(self,dept,fac=None,sec= None) -> None:
+#         self.dept = dept
+#         pass
+#     def __add__(self,other):
+#         self.sum = self.dept + other.dept
+#         return self.sum
+# num1 = Course(10)
+# num2 = Course(30)
+# print(num1+ num2)
+
+
+
+
+# # c1 = Course("CSE110", "TBA", 8)
+# # c1.detail()
+# # print("===============")
+# # c2 = Course("CSE111", "TBA", 9)
+# # c2.detail()
