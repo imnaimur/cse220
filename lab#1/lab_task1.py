cbdf042fe7f76f3410bcce5766bf346094766ace
Naimur <naimur58co@gmail.com>
2023-02-02 20:39:25 +0600

    function added and solved

--- lab#1/lab_task1.py
+++ lab#1/lab_task1.py
@@ -3,24 +3,33 @@
 source=[10,20,30,40,50,60]
 n = len(source)
 k = int(input())
-for j in range(k):
-    for i in range(n-1):
-        source[i] = source[i+1]
-    source[n-j-1] = 0
-print(source)
+
+def shiftLeft(lst,k):
+    for j in range(k):
+        for i in range(n-1):
+            lst[i] = lst[i+1]
+        lst[n-j-1] = 0
+        return(lst)
+
+prothom = (source,k)
+print(prothom)
 
 # roate left k cells
 
 source=[10,20,30,40,50,60]
 n = len(source)
 k = int(input())
-for j in range(k):
-    temp_storage = source[0]
-    for i in range(n-1):
-        source[i] = source[i+1]
-    source[n-1]= temp_storage
 
-print(source)
+def rotateLeft(lst,k):
+    for j in range(k):
+        temp_storage = lst[0]
+        for i in range(n-1):
+            lst[i] = lst[i+1]
+        lst[n-1]= temp_storage
+        return lst
+
+ditio = rotateLeft(source,k)
+print(ditio)
 
 #right shift k cells
 
@@ -28,11 +37,15 @@ source=[10,20,30,40,50,60]
 n = len(source)
 k = int (input())
 
-for j in range(k):
-    for i in range(1,n):
-        source[n-i] = source[n-1-i]
-    source[j] = 0
-print(source)
+def shiftRight(lst,k):
+    for j in range(k):
+        for i in range(1,n):
+            lst[n-i] = lst[n-1-i]
+        lst[j] = 0
+    return lst
+
+tritio = shiftRight(source,k)
+print(tritio)
 
 
 #right rotate k cells
@@ -41,10 +54,29 @@ source=[10,20,30,40,50,60]
 n = len(source)
 k = int(input())
 
-for j in range(k):
-    temp_storage= source[n-1]
-    for i in range(1,n):
-        source[n-i] = source[n-1-i]
-    source[0] = temp_storage
+def rotateRight(lst,k):
+    for j in range(k):
+        temp_storage= lst[n-1]
+        for i in range(1,n):
+            lst[n-i] = lst[n-1-i]
+        lst[0] = temp_storage
+        return lst
 
-print(source)
+choturto = rotateRight(source,k)
+print(choturto)
+
+
+def remove(lst,idx):
+    edt_arr = []
+    for i in range(n):
+        if i != idx:
+            edt_arr +=[lst[i]]
+    edt_arr += [0]
+    return edt_arr
+
+
+source=[10,20,30,40,50,0,0]
+n = len(source)
+idx = int(input())
+ponchom = remove(source,2)
+print(ponchom)
