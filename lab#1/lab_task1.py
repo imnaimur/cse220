de7ca97d9aae8d41ddb9cd85a4103bc46a68c2a0
Naimur <naimur58co@gmail.com>
2023-02-02 19:33:04 +0600

    part 1 no2

--- lab#1/lab_task1.py
+++ lab#1/lab_task1.py
@@ -10,3 +10,13 @@ for j in range(k):
 print(source)
 
 # roate left k cells
+
+source=[10,20,30,40,50,60]
+
+for j in range(k):
+    temp_storage = source[0]
+    for i in range(n-1):
+        source[i] = source[i+1]
+    source[n-1]= temp_storage
+
+print(source)
