d5ed68eade1f07618366ce6b7f7c96d9a1e6ecbc
Naimur <naimur58co@gmail.com>
2023-02-07 09:40:38 +0600

    support

--- Lab#2/circular_rotation.py
+++ Lab#2/circular_rotation.py
@@ -1,9 +1,17 @@
-st = 0
 
-def rotateLeft(lst,n):
-    global st
-    n = len(lst)
+
+def rotateLeft(txt,st):
+    n = len(txt)
     i = 0
     arr = []
     while i < n:
-        arr += []
+        arr += txt[st]
+        st -= 1
+        if st < 0:
+            st = n-1
+        i += 1
+    return arr
+txt1 = rotateLeft("giRtfel2th",2)
+print(txt1)
+text2 = rotateLeft(txt1,0)
+print(text2)
